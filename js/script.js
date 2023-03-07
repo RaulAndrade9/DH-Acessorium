29257e579f2f2a45ce3efd88f702432140a07e79
Raul Andrade <raul.sandrade@hotmail.com>
2023-03-06 22:16:48 -0300

    Telas do PI refeitas

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carrossel').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    
+    });
+  });
