c1655ca507b73d1107c379157e2c8d10e84319f5
Raul Andrade <raul.sandrade@hotmail.com>
2023-03-07 22:51:33 -0300

    inserido eventos com js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,52 @@
+window.addEventListener('load', function() {
+ 
+  const nav = document.querySelector('nav');
+  const links = nav.querySelectorAll('a');
+  const main = document.querySelector('main');
+  const button = document.querySelectorAll('button');
+  const form = this.document.querySelector('.cadastro');
+  const nome = this.document.querySelector('#nome');
+  
+  
+  
+  
+ 
+  links.forEach(function(link) {
+    link.addEventListener('mouseover', function() {
+      link.style.color = '#006400';
+      link.style.fontWeight = 'bold'
+    });
+    
+    
+    link.addEventListener('mouseout', function() {
+      link.style.color = '';
+      link.style.fontWeight = ''
+    });
+  });
+
+ 
+  button.forEach(function(button) {
+    button.addEventListener('mouseover', function(){
+      button.style.backgroundColor = '#00FF00';
+    });
+  
+    button.addEventListener('mouseout', function(){
+      button.style.backgroundColor = '';
+    });
+   
+  });
+
+
+
+  
+
+});
+
+
+
+
+
+/*-------script do carrosel*********/ 
 $(document).ready(function(){
     $('.carrossel').slick({
       dots: true,
@@ -12,11 +61,12 @@ $(document).ready(function(){
   });
 
   
-  
-        const menuToggle = document.querySelector('.menu-toggle');
-        const menu = document.querySelector('.menu');
+  /*-------script do menu hamburger*********/ 
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
       
-        menuToggle.addEventListener('click', () => {
-          menu.classList.toggle('ativo');
+  menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
         });
       
+
